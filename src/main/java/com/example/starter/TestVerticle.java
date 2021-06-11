d3e03703af04373f1f94835a1131bfc0019de757
mjw <13249770806@163.com>
2021-06-11 14:04:59 +0800

    add testverticle

---
+++ src/main/java/com/example/starter/TestVerticle.java
@@ -0,0 +1,24 @@
+package com.example.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class TestVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8888, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("TEST HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
+
